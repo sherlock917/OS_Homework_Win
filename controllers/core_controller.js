8631023c3f872705aba036bcf709eb3175a42f67
Sherlock Zhong <sherlock.z0917@gmail.com>
2014-10-31 17:27:11 +0800

    fix

--- controllers/core_controller.js
+++ controllers/core_controller.js
@@ -4,7 +4,7 @@ var win = gui.Window.get();
 
 var interface_module = require('../modules/interface_module');
 
-var mode = 0;
+var mode = 1;
 
 if (mode == 0) {
   $('.main-screen').css({'opacity' : 1, 'display' : 'block'});
